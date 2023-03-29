c924aea9d1a13a41fa276dc60af0d9662241883e
gollymashed <gm@wassie.games>
2023-03-30 00:18:23 +0100

    Verifying the gift status using the merkle root

--- client/index.js
+++ client/index.js
@@ -1,14 +1,17 @@
-const axios = require('axios');
-const niceList = require('../utils/niceList.json');
-const MerkleTree = require('../utils/MerkleTree');
+const axios = require("axios");
+const niceList = require("../utils/niceList.json");
+const MerkleTree = require("../utils/MerkleTree");
 
-const serverUrl = 'http://localhost:1225';
+const serverUrl = "http://localhost:1225";
 
 async function main() {
-  // TODO: how do we prove to the server we're on the nice list? 
+  const index = Math.floor(Math.random() * niceList.length);
+  const randomName = niceList[index];
+  const proof = new MerkleTree(niceList).getProof(index);
 
   const { data: gift } = await axios.post(`${serverUrl}/gift`, {
-    // TODO: add request body parameters here!
+    name: randomName,
+    proof: proof,
   });
 
   console.log({ gift });
